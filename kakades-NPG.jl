578fa2244b1b70c6a1a4349f323e59257197edf3
Johannes Mueller <johannescmu@aol.de>
2024-02-02 14:30:19 +0100

    Cleaning things up and plotting

--- kakades-NPG.jl
+++ kakades-NPG.jl
@@ -12,31 +12,31 @@ reward(θ) = R(softmaxPolicy(θ), α, γ, μ, r)
 
 nTrajectories = 30;
 θ₀ = randn(nTrajectories, nA*nS);
-nIterations = 3*10^3;
+nIterations = 10^4;
 
-### Run state-action NPG
+### Kakade's NPG
 Δt = 10^-2;
 @elapsed begin
     # Allocate the space for the training trajectories
-    time_Morimura = zeros(nTrajectories, nIterations);
-    rewardTrajectories_Morimura = zeros(nTrajectories, nIterations);
-    policyTrajectories_Morimura = zeros(nTrajectories, nIterations, nA);
-    ηTrajectories_Morimura = zeros(nIterations, nTrajectories, 3);
-    #Optimize using the state-action NPG
+    time_kakade = zeros(nTrajectories, nIterations);
+    rewardTrajectories_kakade = zeros(nTrajectories, nIterations);
+    policyTrajectories_kakade = zeros(nTrajectories, nIterations, nA);
+    ηTrajectories_kakade = zeros(nIterations, nTrajectories, 3);
+    #Optimize using Kakade natural gradient trajectories
     for i in 1:nTrajectories
         θ = θ₀[i,:]
         for k in 1:nIterations
             π = softmaxPolicy(θ)
-            policyTrajectories_Morimura[i, k,:] = π[1, :]
-            rewardTrajectories_Morimura[i, k] = R(π, α, γ, μ, r)
+            policyTrajectories_kakade[i, k,:] = π[1, :]
+            rewardTrajectories_kakade[i, k] = R(π, α, γ, μ, r)
             η = stateActionFrequency(π, α, γ, μ)
-            ηTrajectories_Morimura[k, i, :] = transpose(Bas) * vec(η)
-            G = morimuraConditioner(θ)
+            ηTrajectories_kakade[k, i, :] = transpose(Bas) * vec(η)
+            G = kakadeConditioner(θ)
             Δθ = pinv(G) * ∇R(θ)
             stepsize = Δt / norm(Δθ)
             θ += stepsize * Δθ
             if k < nIterations
-                time_Morimura[i, k+1] =  time_Morimura[i, k] + stepsize
+                time_kakade[i, k+1] =  time_kakade[i, k] + stepsize
             end
         end
     end
@@ -44,19 +44,17 @@ end
 
 title_fontsize, tick_fontsize, legend_fontsize, guide_fontsize = 18, 18, 18, 18;
 
-# Export state-action distribution plot
 begin
-    p = plot(p_state_action_polytope, ηTrajectories_Morimura[:,:,1], ηTrajectories_Morimura[:,:,2], ηTrajectories_Morimura[:,:,3], width=1.5,
+    p = plot(p_state_action_polytope, ηTrajectories_kakade[:,:,1], ηTrajectories_kakade[:,:,2], ηTrajectories_kakade[:,:,3], width=1.5,
         camera = (30, 0), showaxis=false, ticks=false, legend=false, 
         titlefontsize=title_fontsize, tickfontsize=tick_fontsize, legendfontsize=legend_fontsize, 
         guidefontsize=guide_fontsize, size = (400, 400), ylims=(-1.,1.), zlims=(-1.,1.)#, margin=-2cm
     )
     p = plot(p, ηDet_proj[[1, 2, 4, 3, 1],1], ηDet_proj[[1, 2, 4, 3, 1],2], ηDet_proj[[1, 2, 4, 3, 1],3], color="black", width=1.2)
     p = plot(p, Bas[[2, 4],1], Bas[[2, 4],2], Bas[[2, 4],3], color="black", width=1.2, linestyle=:dash)
-    save("graphics/morimura-state-action.pdf", p)
+    save("graphics/kakade-state-action.pdf", p)
 end
 
-# Compute different exponents
 begin
     δ = R_opt - maximum(rewards_det[rewards_det .< maximum(rewards_det)])
     println("δ = ", δ)
@@ -64,23 +62,23 @@ begin
     scaledOptimalityGap = 2 * [(R_opt-rewards_det[j]) / sum(abs.(ηDet[i,:,:] - ηDet[j,:,:])) for j in CartesianIndex.([(1,1), (2,2)])]
     Δ = minimum(scaledOptimalityGap)
     println("Δ = ", Δ)
+    # Δ = 2 * min((R_opt-rewards_det[j]) / sum(abs.(ηDet[i,:,:] - ηDet[j,:,:])), (R_opt-rewards_det[2,2]) / sum(abs.(ηDet[i,:,:] - ηDet[2,2,:,:])))
     A_opt = advantages[i,:,:]
     Δ_M = -(1-γ)^-1 * maximum(A_opt[.! isapprox.(A_opt, 0; atol=10^-10)])
     println("Δ_M = ", Δ_M)
 end
 
-# Export optimality gap plot
 begin
-    time_Morimura[:,1] = minimum(time_Morimura[:,2:end])*ones(nTrajectories)    
-    gap = R_opt*ones(size(transpose(rewardTrajectories_Morimura[:,:,1])))-transpose(rewardTrajectories_Morimura[:,:,1]);
+    time_kakade[:,1] = minimum(time_kakade[:,2:end])*ones(nTrajectories)    
+    gap = R_opt*ones(size(transpose(rewardTrajectories_kakade[:,:,1])))-transpose(rewardTrajectories_kakade[:,:,1]);
     # State-action plot
-    p = plot(transpose(time_Morimura[:,:]), gap, linewidth=1.5);  
+    p = plot(transpose(time_kakade[:,:]), gap, linewidth=1.5);  
     p = plot(p, legend = false, linewidth=1., size=(400,300), fontfamily="Computer Modern", 
         titlefontsize=title_fontsize, tickfontsize=tick_fontsize, legendfontsize=legend_fontsize, guidefontsize=guide_fontsize,
         framestyle=:box, yaxis=:log,
-        ylims=(minimum(gap[10^3,:]),1.2*maximum(gap)), xlims=(minimum(time_Morimura), 0.8*maximum(time_Morimura))
+        ylims=(minimum(gap[10^3,:]),1.2*maximum(gap)), xlims=(minimum(time_kakade), 0.8*maximum(time_kakade))
     );
-    t = range(minimum(time_Morimura), maximum(time_Morimura), 10)
+    t = range(minimum(time_kakade), maximum(time_kakade), 10)
     p = plot(p, t, exp.(- Δ * t), linewidth = 4, color="black", alpha = 0.5, linestyle=:dash)
-    save("graphics/morimura-gap.pdf", p)
+    save("graphics/kakade-gap.pdf", p)
 end
---
+++ utilities.jl
@@ -0,0 +1,70 @@
+using LinearAlgebra
+using ForwardDiff
+
+function valueFunction(π, α, γ, r)
+    (nS, nA) = size(r)
+    pπ = [π[:,s_old]' * α[s_new,s_old,:] for s_new in 1:nS, s_old in 1:nS]
+    rπ = diag(r * π)  # Compute the one step reward
+    Vπ = (I-γ*transpose(pπ))\((1-γ)*rπ)
+    return Vπ
+end
+
+# Reward of a policy
+function R(π, α, γ, μ, r)
+    pπ = [π[:,s_old]' * α[s_new,s_old,:] for s_new in 1:nS, s_old in 1:nS]
+    rπ = diag(r * π)  # Compute the one step reward
+    Vπ = (I-γ*transpose(pπ))\((1-γ)*rπ) # Compute the state value function via Bellman's equation
+    return μ'*Vπ
+end
+
+# State action frequency for a policy
+function stateActionFrequency(π, α, γ, μ)
+    pπ = [π[:,s_old]' * α[s_new,s_old,:] for s_new in 1:nS, s_old in 1:nS]
+    ρ = (I-γ*pπ) \ ((1-γ)*μ)
+    η = Diagonal(ρ) * π'
+    return η
+end
+
+# Define the tabular softmax policy parametrization
+function softmaxPolicy(θ)
+    θ = reshape(θ, (nA, nS))
+    π = exp.(θ)
+    for s in 1:nS
+        π[:,s] = π[:,s] / sum(π[:,s])
+    end
+    return π
+end
+
+# Reward function for the softmax model
+function softmaxReward(θ, α, γ, μ, r)
+    π = softmaxPolicy(θ)
+    return R(π, α, γ, μ, r)
+end
+
+function softmaxStateActionFrequency(θ, α, γ, μ)
+    π = softmaxPolicy(θ)
+    pπ = [π[:,s_old]' * α[s_new,s_old,:] for s_new in 1:nS, s_old in 1:nS]
+    ρ = (I-γ*pπ) \ ((1-γ)*μ)
+    η = Diagonal(ρ) * π'
+    return η
+end
+
+logLikelihoodsSAF(θ) = log.(softmaxStateActionFrequency(θ, α, γ, μ))
+jacobianLogLikelihoodsSAF = θ -> ForwardDiff.jacobian(logLikelihoodsSAF, θ)
+
+function morimuraConditioner(θ)
+    η = vec(softmaxStateActionFrequency(θ, α, γ, μ))
+    J = jacobianLogLikelihoodsSAF(θ)
+    G = [sum(J[:, i].*J[:, j].*η) for i in 1:nP, j in 1:nP]
+    return G
+end
+
+logLikelihoods(θ) = log.(softmaxPolicy(θ))
+jacobianLogLikelihoods = θ -> ForwardDiff.jacobian(logLikelihoods, θ)
+
+function kakadeConditioner(θ)
+    η = vec(softmaxStateActionFrequency(θ, α, γ, μ))
+    J = jacobianLogLikelihoods(θ)
+    G = [sum(J[:, i].*J[:, j].*η) for i in 1:nP, j in 1:nP]
+    return G
+end
